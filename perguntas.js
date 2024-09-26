6f8c388651acd531d3e1cb26216dbc843ede1ada
Mariaafaust <128594235+Mariaafaust@users.noreply.github.com>
2024-09-26 07:30:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,72 @@
+export const perguntas = [
+    
+    {
+        enunciado: "Assim que saimos da escola, temos um caminho para decidir como trilhar, alguns já sabem desde cedo quem querem ser, a profissão que vão seguir, já outros ainda estão na duvida ou ainda não conseguiram decidir. E você? já decidiu?",
+        alternativas: [
+            {
+                texto: "Sim já sei o que vou fazer!",
+                afirmacao: "Eu espero que tenha te ajudado,"
+            },
+            {
+                texto: "Ainda não me decidi",
+                afirmacao: "não tenha medo de se arriscar no mercado de trabalho,"
+            }
+        ]
+    },
+    {
+        enunciado: "Uma das decisões mais importantes na nossa vida é decidir qual carreira seguir. Escolher bem a sua profissão pode proporcionar uma autorrealização, já que você vai estudar o que interessa, trabalhar com o que dá satisfação e não vai passar pelo arrependimento de ter gastado seu tempo numa profissão que não gosta. Para você decidir o que vai fazer primeiro pecisamos entender a area que você prefere!",
+        alternativas: [
+            {
+                texto: "Eu prefiro matematica, tecnologia, ciências humanas...",
+                afirmacao: "Pois só assim você vai conquistar o sucesso!"
+            },
+            {
+                texto: "Eu prefiro ciências humanas, política, piscicologia...",
+                afirmacao: "O futuro é um mundo novo e curioso. "
+            }
+        ]
+    },
+    {
+        enunciado: "Que ótimo! Que você já tem uma base do que seguir, eliminamos a parte mais difícil. Agora precisamos descobrir se você prefere a parte mais burocrática, ou gosta de por a mão na massa. ",
+        alternativas: [
+            {
+                texto: "Gosto de estar por dentro de tudo, de comandar meus grupos de trabalho, mas prefiro ficar na administração de tudo isso, prefiro auxiliar meus colegas com a parte burocrática.",
+                afirmacao: " Cheio de oportunidades!"
+            },
+            {
+                texto: "Gosto de por a mão na massa, estar no meio do público, prefiro contratar alguem de minha confiança para lidar com a papelada, e gosto de cordenar dentro do meu trabalho.",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "É possível que alguns dos seus parentes, amigos, vizinhos ou conhecidos tenham profissões interessantes. Tente lembrar se conhece alguém que sirva como modelo para seguir numa carreira. Informe-se sobre profissões que busquem resolver problemas que você considere relevantes para a sociedade e que também queira ajudar a resolver. Tente descobrir a graduação que eles seguiram, e qual a sua preferência, se gosta de faculdade presencial, semi presencial ou até mesmo um curso tecnico.",
+        alternativas: [
+            {
+                texto: "Prefiro uma faculdade, presencial ou semi presencial.",
+                afirmacao: "Seu futuro sera Brilhante!"
+            },
+            {
+                texto: "Quero um curso tecnico, mais pratico e tem um tempo menor de graduação.",
+                afirmacao: ""
+            },
+            {
+                texto: "Prefiro 100% a distância, posso fazer no conforto de casa ",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "Então agora você já está 98% pronto para eu futuro brilhante, vou deixar aqui pra você dois testes de vocação para te dar uma ajudinha a mais nesta decisão!",
+        alternativas: [
+            {
+                texto: "https://querobolsa.com.br/teste-vocacional-gratis",
+                afirmacao: ""
+            },
+            {
+                texto: "https://www.pravaler.com.br/pra-testar/testes/teste-vocacional-online-gratis/",
+                afirmacao: ""
+            }
+        ]
+    },
+];
